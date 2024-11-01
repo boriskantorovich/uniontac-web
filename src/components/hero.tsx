ad8a1ca6f7342c23739cf9b57d56f79c8fdb2169
bkantorovich <bkantreg@gmail.com>
2024-11-01 16:08:54 +0400

    Fixed hero design

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -32,23 +32,27 @@ export function HeroComponent() {
       />
       <div className="absolute inset-0 bg-black/40" /> 
       
-      <div className="container relative z-10 flex flex-col justify-between h-full py-24">
+      <div className="container relative z-10 flex flex-col justify-between h-full py-12 md:py-24">
         <div className="text-left">
-          <h1 className="text-8xl md:text-8xl lg:text-[10rem] font-bold mb-12 leading-[0.9]">
+          <h1 className="text-8xl md:text-8xl lg:text-[10rem] font-bold mb-8 leading-[0.9]">
             Save a&nbsp;life<br />now
           </h1>
-          <p className="text-3xl mb-32 md:mb-48 font-semibold">
+          <p className="text-3xl mb-16 md:mb-24 font-semibold">
             Send a first aid kit<br /> to those fighting for freedom
           </p>
         </div>
-        <div className="flex flex-col items-start gap-8">
+        <div className="flex flex-col md:flex-row items-start gap-4 mb-8 md:items-center">
           <button 
-            className="w-full md:w-[400px] bg-blue-600 hover:bg-blue-700 text-white font-semibold py-10 rounded-lg text-3xl transition duration-300 mb-8"
+            className="w-full md:w-[400px] bg-blue-600 hover:bg-blue-700 text-white font-semibold py-8 rounded-lg text-3xl transition duration-300"
             onClick={handleDonateClick}
           >
             Help Now!
           </button>
-          <Link href="#learn-more" className="text-2xl underline" onClick={handleLearnMoreClick}>
+          <Link 
+            href="#learn-more" 
+            className="text-2xl underline md:ml-8" 
+            onClick={handleLearnMoreClick}
+          >
             Learn More
           </Link>
         </div>
