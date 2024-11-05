5b6a70ef03c0e7d21db429e458ab4c9769cfd214
bkantorovich <bkantreg@gmail.com>
2024-11-05 14:57:18 +0400

    locale path change

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,21 @@
 import createMiddleware from 'next-intl/middleware';
-import { routing } from './i18n/routing';
+import { defaultLocale, locales } from './i18n/config';
 
-export default createMiddleware(routing);
+export default createMiddleware({
+  // A list of all locales that are supported
+  locales: ['en', 'ru', 'ua'],
+  
+  // Used when no locale matches
+  defaultLocale: 'en',
+  
+  // English will be served from the root (/)
+  localePrefix: 'as-needed',
+  
+  // Alternative to localePrefix that gives more fine-grained control
+  localeDetection: false
+});
 
 export const config = {
   // Match only internationalized pathnames
-  matcher: ['/', '/(en|ua|ru)/:path*', '/((?!api|_next|.*\\..*).*)']
+  matcher: ['/', '/(ru|ua)/:path*', '/((?!api|_next|_vercel|.*\\..*).*)']
 }; 
