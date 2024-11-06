e04147b29ec915f9321ae610d1dfe1e54141ff6d
bkantorovich <bkantreg@gmail.com>
2024-11-06 12:52:58 +0400

    preload optimizations

--- next.config.mjs
+++ next.config.mjs
@@ -8,13 +8,65 @@ const nextConfig = {
   images: {
     domains: ['uniontacua.com'],
   },
-  webpack(config) {
+  swcMinify: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
+  experimental: {
+    optimizePackageImports: [
+      '@/components/ui',
+      'intl-messageformat',
+      '@formatjs/intl-localematcher'
+    ],
+    optimizeCss: true,
+    serverActions: {
+      bodySizeLimit: '2mb',
+    },
+  },
+  webpack: (config, { dev, isServer }) => {
     config.module.rules.push({
       test: /\.(woff|woff2|eot|ttf|otf)$/i,
       type: 'asset/resource',
     });
+
+    if (!dev && !isServer) {
+      config.optimization = {
+        ...config.optimization,
+        runtimeChunk: 'single',
+        splitChunks: {
+          chunks: 'all',
+          maxInitialRequests: 25,
+          minSize: 20000,
+          cacheGroups: {
+            framework: {
+              name: 'framework',
+              test: /[\\/]node_modules[\\/](react|react-dom|next|@next)[\\/]/,
+              priority: 40,
+              enforce: true,
+            },
+            lib: {
+              test: /[\\/]node_modules[\\/]/,
+              name: 'lib',
+              priority: 30,
+              enforce: true,
+            },
+            commons: {
+              name: 'commons',
+              minChunks: 2,
+              priority: 20,
+            },
+            intl: {
+              name: 'intl',
+              test: /[\\/]node_modules[\\/](intl-messageformat|@formatjs)[\\/]/,
+              priority: 50,
+              enforce: true,
+            }
+          }
+        }
+      };
+    }
     return config;
-  },
+  }
 };
 
 export default withNextIntl(nextConfig);
